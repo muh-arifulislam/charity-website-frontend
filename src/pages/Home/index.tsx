069ebb2a2817321b5d7897e094e9197bb51a69a9
muh-arifulislam <arifibnenam@gmail.com>
2024-10-07 04:33:29 +0600

    new section added on homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,9 @@ import cardImage from "../../assets/images/girls.png";
 import { FaCheckCircle, FaPlay } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
+import heroBgImg from "../../assets/images/heroBg-home.png";
+import heroImg from "../../assets/images/image1.png";
+
 const Home = () => {
   return (
     <div className="">
@@ -165,55 +168,129 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <div className="grid grid-cols-1 sm:grid-cols-2">
-        <div className="py-[74px] px-4 sm:px-20 relative bg-primary">
-          <div className="">
-            <p className="text-[35px] sue-ellen-francisco-regular">Subscribe</p>
-            <h5 className="text-[40px] shippori-mincho-extrabold mb-4">
-              Newsletter
-            </h5>
-            <p className="">
-              Sign up for our monthly newsletter to get the latest news,
-              volunteer opportunities,
-            </p>
-            <div>
-              <form action="" className="flex gap-6">
-                <input
-                  className="px-6 py-5 rounded-[34px] w-full outline-none"
-                  type="text"
-                  placeholder="Enter Your Email Address"
-                />
-                <input
-                  type="submit"
-                  value="SEND"
-                  className="px-8 py-5 bg-accent rounded-[34px]"
-                />
+      <section
+        className="py-[135px] relative z-0  w-full bg-cover bg-center bg-no-repeat  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-overlay-accent before:-z-10 before:backdrop-blur-sm"
+        style={{ backgroundImage: `url(${heroBgImg})` }}
+      >
+        <div className="max-w-screen-xl mx-auto px-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-[56px] relative">
+            <div className="text-white">
+              <p className="text-[35px] sue-ellen-francisco-regular leading-[108%]mb-[30px]">
+                How We Help
+              </p>
+              <p className="text-[40px] shippori-mincho-extrabold leading-[120%] mb-[30px]">
+                Join The CommunityTo Give Education For Children
+              </p>
+              <p className="text-xl leading-[130%] mb-[30px] text-slate-300">
+                Lorem Ipsum is simply dummy text of the printin typesetting
+                dummy text ever when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+              </p>
+              <div className="mb-[30px]">
+                <img src={heroImg} alt="" />
+              </div>
+              <div>
+                <Button variant="primary">Donate Now</Button>
+              </div>
+            </div>
+            <div className="border px-5 sm:px-[63px] py-[48px]">
+              <h4 className="text-center text-2xl sm:text-[35px] shippori-mincho-extrabold leading-[120%] mb-[30px] text-white">
+                Become A Volunteer
+              </h4>
+              <form action="">
+                <div className="w-full mb-[30px]">
+                  <input
+                    className="w-full border bg-transparent p-[24px] outline-none text-white text-lg"
+                    type="text"
+                    placeholder="Full Name*"
+                  />
+                </div>
+                <div className="w-full mb-[30px]">
+                  <input
+                    className="w-full border bg-transparent p-[24px] outline-none text-white text-lg"
+                    type="text"
+                    placeholder="Email Address*"
+                  />
+                </div>
+                <div className="w-full mb-[30px]">
+                  <input
+                    className="w-full border bg-transparent p-[24px] outline-none text-white text-lg"
+                    type="text"
+                    placeholder="Phone Number*"
+                  />
+                </div>
+                <div className="w-full mb-[30px] h-auto">
+                  <textarea
+                    className="w-full border bg-transparent p-[24px] outline-none text-white text-lg"
+                    placeholder="Message*"
+                  ></textarea>
+                </div>
+
+                <div></div>
+
+                <div>
+                  <Button variant="white" type="submit" className="px-8">
+                    Submit Now
+                  </Button>
+                </div>
               </form>
             </div>
           </div>
         </div>
-        <div>
-          <div
-            className="min-h-[300px] sm:h-full z-0  w-full bg-cover bg-center bg-no-repeat relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-overlay-secondary before:-z-10"
-            style={{
-              backgroundImage: `url(${image})`,
-            }}
-          >
-            <div className="py-[74px] px-4 sm:px-20   w-full h-full  z-50">
+      </section>
+      <section>
+        <div className="grid grid-cols-1 sm:grid-cols-2">
+          <div className="py-[74px] px-4 sm:px-20 relative bg-primary">
+            <div className="">
               <p className="text-[35px] sue-ellen-francisco-regular">
-                Recent Blog
+                Subscribe
               </p>
               <h5 className="text-[40px] shippori-mincho-extrabold mb-4">
-                Take Look At Recent Blog <br />
-                Posts
+                Newsletter
               </h5>
-              <button className="px-8 py-5 bg-primary rounded-[34px]">
-                SEE OUR BLOGS
-              </button>
+              <p className="">
+                Sign up for our monthly newsletter to get the latest news,
+                volunteer opportunities,
+              </p>
+              <div>
+                <form action="" className="flex gap-6">
+                  <input
+                    className="px-6 py-5 rounded-[34px] w-full outline-none"
+                    type="text"
+                    placeholder="Enter Your Email Address"
+                  />
+                  <input
+                    type="submit"
+                    value="SEND"
+                    className="px-8 py-5 bg-accent rounded-[34px]"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className="min-h-[300px] sm:h-full z-0  w-full bg-cover bg-center bg-no-repeat relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-overlay-secondary before:-z-10"
+              style={{
+                backgroundImage: `url(${image})`,
+              }}
+            >
+              <div className="py-[74px] px-4 sm:px-20   w-full h-full  z-50">
+                <p className="text-[35px] sue-ellen-francisco-regular">
+                  Recent Blog
+                </p>
+                <h5 className="text-[40px] shippori-mincho-extrabold mb-4">
+                  Take Look At Recent Blog <br />
+                  Posts
+                </h5>
+                <button className="px-8 py-5 bg-primary rounded-[34px]">
+                  SEE OUR BLOGS
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
